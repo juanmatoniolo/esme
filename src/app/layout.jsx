16dafbde45d5a53254706800b9fae5c943aed17e
juanmatoniolo <juanmatoniolo2@gmail.com>
2025-09-18 02:57:15 -0300

    metadata

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,11 +3,10 @@ import "./globals.css";
 import { Nunito, Dancing_Script } from "next/font/google";
 import BootstrapClient from "@/components/Boostrap/BootstrapClient";
 
-// Configuración tipografías
 const nunito = Nunito({
   subsets: ["latin"],
   variable: "--font-nunito",
-  weight: ["400", "500", "600"],
+  weight: ["400", "500", "600", "700"],
 });
 
 const dancingScript = Dancing_Script({
@@ -16,20 +15,56 @@ const dancingScript = Dancing_Script({
   weight: ["400", "500", "600", "700"],
 });
 
-// Metadata SEO
 export const metadata = {
-  title: "Esmeralda Perisset | Psicóloga Clínica - Talleres y Libro",
+  title: "Esmeralda Perisset | Psicóloga Clínica",
   description:
-    "Soy Esmeralda, psicóloga clínica. Descubrí mi curso online, talleres de autoconocimiento y mi libro 'Líneas sinceras'.",
+    "Soy Esmeralda, psicóloga clínica. Descubrí mis talleres de autoconocimiento y mi libro 'Líneas sinceras'.",
+
+  // ✅ Íconos
+  icons: {
+    icon: "/icon_32x32.png",
+    shortcut: "/icon_96x96.png",
+    apple: "/icon_192x192.png"
+  },
+
+  // ✅ PWA Manifest
+  manifest: "/manifest.json",
+
+  // ✅ Metadata para compartir en redes
+  openGraph: {
+    title: "Esmeralda Perisset | Psicóloga Clínica",
+    description:
+      "Talleres de autoconocimiento y el libro 'Líneas sinceras'.",
+    url: "psicoesmeperisset.com",
+    siteName: "Esmeralda Perisset",
+    images: [
+      {
+        url: "/icon_1024x1024.png",
+        width: 1024,
+        height: 1024,
+        alt: "Logo Esmeralda Perisset"
+      }
+    ],
+    locale: "es_ES",
+    type: "website"
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Esmeralda Perisset | Psicóloga Clínica",
+    description: "Talleres de autoconocimiento y el libro 'Líneas sinceras'.",
+    images: ["/icon_1024x1024.png"],
+    creator: "@tuusuario"
+  }
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es" className={`${nunito.variable} ${dancingScript.variable}`}>
-      <body>
+      <body suppressHydrationWarning={true}>
         <BootstrapClient />
-        {children}</body>
+        {children}
+      </body>
     </html>
   );
 }
-
